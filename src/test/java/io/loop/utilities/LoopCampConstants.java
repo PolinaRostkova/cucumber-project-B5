69ead7b3b410b15b37e4cc7c84cb5f2ccc96c9e4
PolinaRostkova <polina.rostkova02@gmail.com>
2025-09-04 22:16:11 -0400

    day 11

---
+++ src/test/java/io/loop/utilities/LoopCampConstants.java
@@ -0,0 +1,8 @@
+package io.loop.utilities;
+
+public class LoopCampConstants {
+    public static final String URL_REGISTRATION_FORM = "https://loopcamp.vercel.app/registration_form.html";
+    public static final String RADIO_BUTTON_URL = "https://loopcamp.vercel.app/radio-buttons.html";
+    public static final String EXPECTED_HEADER_FOR_FORM = "Registration form";
+    public static final String EXPECTED_PLACEHOLDER_FOR_FIRSTNAME = "first name";
+}
